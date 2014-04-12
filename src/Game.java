6296eb05a3bd75b15419bac5ea05d592f2bac3f5
JackNeus <jack.neus@gmail.com>
2014-04-12 17:26:10 -0400

    This probably won't work

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 public class Game {
 	public static void main(String args[]) {
 		System.out.println("Abi sucks");
+		System.out.println("Abi is ok");
 	}
 
 	public static void update() // function triggered every frame
