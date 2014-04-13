7287d53ccdf33b4d90b2ad42ffa1f4168c7cb4cd
JackNeus <jack.neus@gmail.com>
2014-04-12 21:33:21 -0400

    Initializing stuff

--- src/Maze.java
+++ src/Maze.java
@@ -1,26 +1,34 @@
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class Maze extends JFrame {
 	/*Window Variables*/
 	public static int width = 800, height = 600, margin = 100;
 	public static int tileSize = 20;
 	public static int xTiles = width / tileSize, yTiles = height / tileSize;
+	public static int numNodes;
 	
 	public static boolean adj[][];
-	
+	public static Node nodes[];
 	
 	public Maze(){	
 		setTitle("Maze Game");
 		setSize(width, height);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBackground(Color.RED);
 		
-		adj = new boolean[xTiles * yTiles][xTiles * yTiles];
+		numNodes = xTiles * yTiles;
+		adj = new boolean[numNodes][numNodes];
+		nodes = new Node[numNodes];
+		for(int i = 0; i < numNodes; i++){
+			nodes[i] = new Node(i % xTiles, i / yTiles, i);
+		}
 		
 		setVisible(true);
 	}
 	
-	public static void main(){
+	public static void main(String args[]){
 		new Maze();
 	}
 }
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,10 @@
 public class Node {
 	int x, y;
 	int id;
+	
+	public Node(int x, int y, int id){
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
 }
