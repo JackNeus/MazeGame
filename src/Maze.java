dd04bb1fe83bbca000308e5c667c6beed26902d0
AbiSri <AbiSri@bergen.org>
2014-04-13 04:50:31 -0400

    Maze Solver class

--- src/Maze.java
+++ src/Maze.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
 public class Maze extends JFrame {
 	/*Window Variables*/
 	public int width = 600, height = 400, margin = 100, adjusty, adjustx;
-	public int tileSize = 10;
+	public int tileSize = 5;
 	public int xTiles = width / tileSize, yTiles = height / tileSize;
 	public int numNodes;
 
@@ -19,11 +19,12 @@ public class Maze extends JFrame {
 	public boolean[][] adj;
 	public Node[] nodes;
 	
-	public Maze(){	
+	public Maze() {	
 		setTitle("Maze Game");
 		
 		JPanel jp = new JPanel();
 		jp.setPreferredSize(new Dimension(margin + width + margin, margin + height + margin));
+		jp.setBackground(Color.GREEN);
 		this.getContentPane().add(jp);
 		this.pack();
 		adjusty = margin + getInsets().top;
@@ -31,16 +32,16 @@ public class Maze extends JFrame {
 		
 		setResizable(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		getContentPane().setBackground(Color.WHITE);
+		getContentPane().setBackground(Color.GREEN);
 		
 		numNodes = xTiles * yTiles;
 		adj = new boolean[numNodes][numNodes];
 		nodes = new Node[numNodes];
-		for(int i = 0; i < numNodes; i++){
+		for(int i = 0; i < numNodes; i++) {
 			nodes[i] = new Node(i % xTiles, i / xTiles, i);
 		}
-		for(int i = 0; i < numNodes; i++){
-			for(int j = 0; j < numNodes; j++){
+		for(int i = 0; i < numNodes; i++) {
+			for(int j = 0; j < numNodes; j++){ 
 				adj[i][j] = false;
 			}
 		}
@@ -52,10 +53,10 @@ public class Maze extends JFrame {
 	}
 	
 	int dirs[];
-	public void genMaze(){
+	public void genMaze() {
 		dirs = new int[]{-1, 1, xTiles, -xTiles};
 		
-		while(joiner.components > 1){
+		while(joiner.components > 1) {
 			int id = (int) Math.floor(Math.random() * numNodes);
 			int dir = (int) Math.floor(Math.random() * 4);
 			
@@ -82,17 +83,17 @@ public class Maze extends JFrame {
 	}
 	
 	public Line2D[] lines;
-	public void genLines(){
+	public void genLines() {
 		lines = new Line2D[(xTiles + 1) * (yTiles + 1) * 2];
 		int pos = 0;
-		for(int i = 0; i < numNodes; i++){
-			if(nodes[i].x == 0 || !adj[i][i - 1]){
+		for(int i = 0; i < numNodes; i++) {
+			if(nodes[i].x == 0 || !adj[i][i - 1]) {
 				lines[pos++] = new Line2D.Float(nodes[i].x * tileSize + adjustx,
 												nodes[i].y * tileSize + adjusty,
 												nodes[i].x * tileSize + adjustx,
 												nodes[i].y * tileSize + adjusty + tileSize);
 			}
-			if(nodes[i].y == 0 || !adj[i][i - xTiles]){
+			if(nodes[i].y == 0 || !adj[i][i - xTiles]) {
 				lines[pos++] = new Line2D.Float(nodes[i].x * tileSize + adjustx,
 												nodes[i].y * tileSize + adjusty,
 												nodes[i].x * tileSize + adjustx + tileSize,
@@ -100,13 +101,13 @@ public class Maze extends JFrame {
 
 			}
 		}
-		for(int i = 0; i < xTiles; i++){
+		for(int i = 0; i < xTiles; i++) {
 			lines[pos++] = new Line2D.Float(i * tileSize + adjustx,
 											adjusty + height,
 											i * tileSize + adjustx + tileSize,
 											adjusty + height);
 		}
-		for(int i = 0; i < yTiles; i++){
+		for(int i = 0; i < yTiles; i++) {
 			lines[pos++] = new Line2D.Float(adjustx + width,
 											i * tileSize + adjusty,
 											adjustx + width,
@@ -116,9 +117,12 @@ public class Maze extends JFrame {
 		//lines[pos++] = new Line2D.Float(adjustx + width, adjusty, adjustx + width, adjusty + height);
 	}
 	
-	public void paint(Graphics g){
+	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.WHITE);
+		g2.fillRect(adjustx, adjusty, width, height);
+		g2.setColor(Color.BLACK);
 		for(int i = 0; i < lines.length; i++){
 			if(lines[i] != null){
 				g2.draw(lines[i]);
