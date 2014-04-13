a4eef8f7cf2fc45234729a1c85f635452dda551e
JackNeus <jack.neus@gmail.com>
2014-04-12 21:24:04 -0400

    Work

--- src/Maze.java
+++ src/Maze.java
@@ -1,14 +1,23 @@
 import javax.swing.JFrame;
 
 public class Maze extends JFrame {
-	public static int width = 800, height = 600;
+	/*Window Variables*/
+	public static int width = 800, height = 600, margin = 100;
+	public static int tileSize = 20;
+	public static int xTiles = width / tileSize, yTiles = height / tileSize;
 	
-	public Maze(){
+	public static boolean adj[][];
+	
+	
+	public Maze(){	
 		setTitle("Maze Game");
 		setSize(width, height);
 		setResizable(false);
-		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		adj = new boolean[xTiles * yTiles][xTiles * yTiles];
+		
+		setVisible(true);
 	}
 	
 	public static void main(){
