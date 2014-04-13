0758782899a3794fc4a930620dd2c25ca726fc4e
AbiSri <AbiSri@bergen.org>
2014-04-12 21:21:50 -0400

    Node class

--- src/Maze.java
+++ src/Maze.java
@@ -2,16 +2,16 @@ import javax.swing.JFrame;
 
 public class Maze extends JFrame {
 	public static int width = 800, height = 600;
-	
-	public Maze(){
+
+	public Maze() {
 		setTitle("Maze Game");
 		setSize(width, height);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	public static void main(){
+
+	public static void main() {
 		new Maze();
 	}
 }
