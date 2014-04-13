3f778174263ec7f0f14fd7c22165a03acc4eca48
JackNeus <jack.neus@gmail.com>
2014-04-13 09:17:56 -0400

    RObot stuff

--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,22 @@
-
 public class Robot {
+	int[] dirx = new int[]{1, 0, -1, 0};
+	int[] diry = new int[]{0, -1, 0, 1};
+	Maze maze;
+	
+	int pos;
 	int dir = 0;
+	
+	public Robot(Maze maze){
+		this.maze = maze;
+		pos = maze.entry;
+		dir = 0;
+	}
+	
+	public solve(){
+		
+		
+	}
+	
+	
 }
+
